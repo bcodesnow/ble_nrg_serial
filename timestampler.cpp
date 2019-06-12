9db04e4d9d23388a90617cebd6e85e5ae36fcd63
b <bcodesnow@github.com>
2019-06-12 21:31:28 +0200

    added the last states.

--- timestampler.cpp
+++ timestampler.cpp
@@ -9,9 +9,9 @@ TimeStampler::TimeStampler(QObject *parent): m_deviceHandler(0)
     connect(&m_timeout_timer, SIGNAL(timeout()), this, SLOT(timeout_timer_expired()) );
     connect(&m_send_timer, SIGNAL(timeout()), this, SLOT(send_timer_expired()) );
     m_send_timer.setSingleShot(false);
-    m_send_timer.setInterval(33);
+    m_send_timer.setInterval(TS_SENDING_PERIOD_MS);
     m_timeout_timer.setSingleShot(true);
-    m_timeout_timer.setInterval(25);
+    m_timeout_timer.setInterval(TS_TIMEOUT_DELAY_MS);
 }
 
 void TimeStampler::time_sync_msg_sent(QByteArray msg)
@@ -19,9 +19,9 @@ void TimeStampler::time_sync_msg_sent(QByteArray msg)
     m_last_msg_ts = get_timestamp_us();
 }
 
-uint32_t TimeStampler::get_diff_in_us_to_current_ts(uint32_t someTimeStamp)
+uint32_t TimeStampler::get_diff_in_us_to_current_ts(uint32_t some_ts)
 {
-    return ( ( m_etimer.nsecsElapsed() / 1000 ) - someTimeStamp );
+    return ( ( m_etimer.nsecsElapsed() / 1000 ) - some_ts );
 }
 
 void TimeStampler::setRefToDevHandlerArr(DeviceHandler *dev_handler_arr)
@@ -85,14 +85,22 @@ void TimeStampler::time_sync_msg_arrived(QByteArray msg)
     switch (m_sync_state)
     {
     case START_WAITS_FOR_ACK:
-        m_send_timer.start();
+        if (msg.at(2) != TS_CMD_ACK)
+        {
+            m_sync_state = STOPPED;
+            qCritical()<<"TS: Wrong msg arrived.. we were waiting for an ACK..";
+        }
+        else
+        {
+            // all good, lets go!)
+            m_send_timer.start();
+        }
         break;
     case MEASURING_TRAVELING_TIME:
-        travelling_times.append( get_diff_in_us_to_current_ts( m_last_msg_ts) );
+        travelling_times.append( get_diff_in_us_to_current_ts( m_last_msg_ts ) );
         if (msg.at(2) != m_send_timer_repeat_count)
             qCritical()<<"Shit got fkd up during time sync.. msgcount error!";
         // TODO: also log the processing times!
-
         break;
     case CALCULATING_COMPENSATION:
     {
@@ -105,13 +113,35 @@ void TimeStampler::time_sync_msg_arrived(QByteArray msg)
 
         m_travelling_time_acceptance_trsh = min * 1.1;
 
+
         m_sync_state = SENDING_COMPENSATED;
         m_send_timer.start();
-
         break;
     }
     case SENDING_COMPENSATED:
         if (  get_diff_in_us_to_current_ts( m_last_msg_ts ) < m_travelling_time_acceptance_trsh )
+        {
+            // This was a good one!
+            m_send_timer.stop();
+            QByteArray tba;
+            tba.resize(2);
+            tba[0] = TS_MSG;
+            tba[1] = TS_CMD_LAST_ONE_WAS_GOOD_ONE;
+            m_deviceHandler[m_dev_idx_in_sync].ble_uart_tx(tba);
+        }
+        break;
+    case STOP_WAITS_FOR_ACK:
+        if (msg.at(2) != TS_CMD_ACK)
+        {
+            m_sync_state = STOPPED;
+            qCritical()<<"TS: Wrong msg arrived.. we were waiting for an ACK..";
+        }
+        else
+        {
+            // we did it!
+            qInfo()<<"TS: Sync completed!";
+            emit time_sync_completed();
+        }
         break;
     }
 }
@@ -153,21 +183,19 @@ void TimeStampler::send_timer_expired()
             // last msg sent
             m_send_timer_repeat_count = 0;
             m_sync_state = CALCULATING_COMPENSATION;
+            qCritical()<<"TS: Max retry reached, failed!";
+            emit time_sync_failed();
         }
         else
         {
             send_compensated_time_sync_msg();
         }
     }
-    else
-    {
-        // start or some other not interesting state))
-    }
 }
 
 void TimeStampler::timeout_timer_expired()
 {
-    qCritical()<<"TimeSync Failed - > Timeout!";
-    m_sync_state = UNUSED;
+    qCritical()<<"TimeSync Failed - > Timeout! - > Current State : "<<m_sync_state;
+    m_sync_state = STOPPED;
 }
 
--- timestampler.h
+++ timestampler.h
@@ -15,16 +15,17 @@
  *          -> TimeSync -vPushalike NTP ove BLE UART
  *          -> Travelling Time Measurement
  */
+#define TS_MEASURE_MSG_CNT          35
+#define TS_COMPENSATED_MSG_CNT      50
+#define TS_SENDING_PERIOD_MS        30
+#define TS_TIMEOUT_DELAY_MS         25
 
 #define START_WAITS_FOR_ACK         (1<<0)
 #define MEASURING_TRAVELING_TIME    (1<<1)
-#define UNUSED                      (1<<2)
+#define STOPPED                     (1<<2)
 #define CALCULATING_COMPENSATION    (1<<3)
 #define SENDING_COMPENSATED         (1<<4)
-
-#define TS_MEASURE_MSG_CNT          35
-#define TS_COMPENSATED_MSG_CNT      50
-#define TS_SENDING_PERIOD_MS        25
+#define STOP_WAITS_FOR_ACK          (1<<5)
 
 class DeviceHandler;
 
@@ -51,7 +52,6 @@ private:
 signals:
     void time_sync_completed();
     void time_sync_failed();
-    void last_ts_msg_sent();
 
 public:
     TimeStampler(QObject *parent = 0);
@@ -62,7 +62,7 @@ public:
     uint32_t get_timestamp_ms();
     uint32_t get_timestamp_us();
 
-    uint32_t get_diff_in_us_to_current_ts ( uint32_t someTimeStamp );
+    uint32_t get_diff_in_us_to_current_ts ( uint32_t some_ts );
     void start_time_sync(quint8 devIdxToSync);
 
 public slots:
@@ -70,7 +70,6 @@ public slots:
     void time_sync_msg_sent(QByteArray msg);
     void send_timer_expired();
     void timeout_timer_expired();
-
 };
 
 #endif // TIMESTAMPLER_H
